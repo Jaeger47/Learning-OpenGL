08b090333c1be4ed841038cd1aac67894ada5391
Mark Daniel G. Dacer <markdacer21@gmail.com>
2020-05-27 09:05:40 +0800

    Update hello_world.cpp

--- Computer Graphics Programming Using C++ and OpenGL/hello_world.cpp
+++ Computer Graphics Programming Using C++ and OpenGL/hello_world.cpp
@@ -1,7 +1,7 @@
 // hello_window.cpp
 //
 // created by Mark Daniel G. Dacer at BukSU
-// https://github.com/Jaeger47/Learning-OpenGL
+// https://jaeger47.github.io/Learning-OpenGL/
 //
 
 #include <GL/glut.h> // OpenGL Utility 
@@ -14,7 +14,7 @@ int main(int argc,char**argv)
 	glutInitDisplayMode(GLUT_RGB); //sets diplay to rgb
 	glutInitWindowPosition(200,100); //position of the program in your screen
 	glutInitWindowSize(500,500); //size of the program window
-    glutCreateWindow("Hello Window"); //window title
+    	glutCreateWindow("Hello Window"); //window title
 	glutDisplayFunc(display);//call display function
 
 	glutMainLoop(); //loop all functions
