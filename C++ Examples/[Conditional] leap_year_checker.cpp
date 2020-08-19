8cafd8e0fcbe5cde409ae88cbcd6ce12e5a835a6
Mark Daniel G. Dacer <markdacer21@gmail.com>
2020-08-19 09:35:40 +0800

    Update [Conditional] leap_year_checker.cpp

--- C++ Examples/[Conditional] leap_year_checker.cpp
+++ C++ Examples/[Conditional] leap_year_checker.cpp
@@ -10,7 +10,7 @@ void main()
 	cout << "Enter a year: ";
 	cin >> year;
 	if ( ( year%4==0 && year%100!=0 ) || year%400==0 )
-		cout << year << " is a leap year.";
+		cout << year << " is a leap year." << endl;
 	else
 		cout << year << " is not a leap year." << endl;
 
